d4243180dbdc47869bbcf102ac8b795931435317
rashed800 <68921917+rashed800@users.noreply.github.com>
2020-09-21 11:33:11 +0600

    File upload

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    //Typed Js
+	var typed = new Typed('.typed', {
+    // Waits 1000ms after typing "First"
+	strings: ['NOOR-E ADON MONTAHA', 'TO WHOM I COMPLETELY MELTED <i class="fa fa-heart" style="font-size:48px; color: red"></i>'],
+	typeSpeed: 120,
+	startDelay: 1000,
+	loop: true,
+	showCursor: false,
+
+ });
+    
+    
+    
+});
